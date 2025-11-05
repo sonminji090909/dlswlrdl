86688aaeaa7ad4ed72c8149b17593f67a453eeb2
sonminji090909 <sonminji303@gmail.com>
2025-11-05 09:24:41 +0900

    Change output message to use warning and error

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ a=st.text_input('왓츄어네임')
 b=st.selectbox('좋아하는 음식이 뭐긔?',['국밥','새우','족발'])
 if st.button('인사말 생성'):
    st.write(a+'님아 안녕하시긔')
-st.write('b+'를 좋아하노? 난 싫어하긔;')
+   st.warning(b+'를 좋아하노? 난 싫어하긔;')
+   st.error('반갑진 않긔')
